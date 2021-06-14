c35c355ca46eb189052472d3c2622d75a13b95e9
Nataliesmyth <nataliesmyth11@gmail.com>
2021-06-14 11:55:03 -0400

    fetching data with axios

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,12 +1,33 @@
-// containers/TodosContainer.js
 import React, { Component } from 'react';
+import TodoModel from '../models/Todo';
+import Todos from '../components/Todos';
 
 class TodosContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    };
+  };
+  
+  componentDidMount() {
+    this.fetchData();
+  };
+  
+  fetchData = () => {
+    TodoModel.all().then((res) => {
+      this.setState ({
+        todos: res.data.todos,
+      });
+    });
+  };
+  
   render() {
     return (
-        <div className='todosContainer'>
-            <h2>This is the todos container</h2>
-        </div>
+      <div className="todosComponent">
+        <Todos
+          todos={this.state.todos} />
+      </div>
     );
   };
 };
