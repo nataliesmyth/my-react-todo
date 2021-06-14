93c4e6f9fb5670e0b08fc1e734ceebce4fe5fb5b
Nataliesmyth <nataliesmyth11@gmail.com>
2021-06-13 22:26:39 -0400

    home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import TodosContainer from './containers/TodosContainer';
 
 function App() {
   return (
-      <div className="container">
-        <h1>Hello World</h1>
+    <div className="container">
+      <Switch>
+        <Route exact path='/' component={ Home }/>
+        <Route path='/todos' component={ TodosContainer }/>
+      </Switch>
     </div>
   );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+// components/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <h2>
+      I am the Home page
+    </h2>
+  );
+};
+
+export default Home;
