a427c61c85a3d65e9956477692a93d0e6db81738
Nataliesmyth <nataliesmyth11@gmail.com>
2021-06-14 00:07:27 -0400

    router setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import TodosContainer from './containers/TodosContainer';
+import Header from './components/Header';
+import routes from './config/routes';
 
 function App() {
   return (
     <div className="container">
-      <Switch>
-        <Route exact path='/' component={ Home }/>
-        <Route path='/todos' component={ TodosContainer }/>
-      </Switch>
+      <Header/>
+      { routes }
     </div>
   );
 };
